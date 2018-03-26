4f832345145f7d97a5b8faf495c29f4c68cb3837
LiangziKing <1990818918@qq.com>
2018-03-26 12:42:23 +0800

    add samples/module

---
+++ samples/module/hello.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-26 12:36:42
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-26 12:37:52
+
+' a test module '
+
+__author__ = 'LiangziKing'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args)==1:
+            print('Hello, world!')
+    elif len(args)==2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+if __name__=='__main__':
+    test()
