411a7d4e38b83d53d9222e4abe6342f9fbb0720d
LiangziKing <1990818918@qq.com>
2018-03-26 14:13:50 +0800

    add samples/packages/pil

---
+++ samples/packages/pil/use_pil_blur.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-26 13:59:28
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-26 14:04:22
+
+from PIL import Image, ImageFilter
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('test.jpg')
+# 应用模糊滤镜:
+im2 = im.filter(ImageFilter.BLUR)
+im2.save('blur.jpg', 'jpeg')
---
+++ samples/packages/pil/use_pil_resize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-26 14:00:20
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-26 14:08:28
+
+from PIL import Image
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('test.jpg')
+# 获得图像尺寸:
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+# 缩放到50%:
+im.thumbnail((w//2, h//2))
+print('Resize image to: %sx%s' % (w//2, h//2))
+# 把缩放后的图像用jpeg格式保存:
+im.save('thumbnail.jpg', 'jpeg')
