8f626ed060580d887c4b4875abe3376e35611d67
LiangziKing <1990818918@qq.com>
2018-03-27 09:50:02 +0800

    add samples/socket

---
+++ samples/socket/udp_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-27 09:46:08
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-27 09:48:47
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.sendto(data, ('127.0.0.1', 9999))
+    # 接收数据:
+    print(s.recv(1024).decode('utf-8'))
+
+s.close()
